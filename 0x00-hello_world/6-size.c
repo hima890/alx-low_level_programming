7d5dab29ba8c124c767de62347f6607c7d375613
hima890 <hfibrahim90@gmail.com>
2023-09-07 17:10:32 +0200

    Add file 6 agin 3

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-printf("Size of char: %zu bytes\n", sizeof(char));
+printf("Size of char: %zu bytes(s)\n", sizeof(char));
 printf("Size of int: %zu bytes(s)\n", sizeof(int));
 printf("Size of long int: %zu bytes(s)\n", sizeof(long int));
 printf("Size of long long int: %zu bytes(s)\n", sizeof(long long int));
