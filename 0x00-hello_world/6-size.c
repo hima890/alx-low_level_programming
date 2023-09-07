53cc0351f9bd88d42e129248c56a92e6b5b290a4
hima890 <hfibrahim90@gmail.com>
2023-09-07 17:09:11 +0200

    Add file 6 agin 2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 int main(void)
 {
 printf("Size of char: %zu bytes\n", sizeof(char));
-printf("Size of int: %zu bytes\n", sizeof(int));
-printf("Size of long int: %zu bytes\n", sizeof(long int));
-printf("Size of long long int: %zu bytes\n", sizeof(long long int));
-printf("Size of float: %zu bytes\n", sizeof(float));
+printf("Size of int: %zu bytes(s)\n", sizeof(int));
+printf("Size of long int: %zu bytes(s)\n", sizeof(long int));
+printf("Size of long long int: %zu bytes(s)\n", sizeof(long long int));
+printf("Size of float: %zu bytes(s)\n", sizeof(float));
 
 return (0);
 }
