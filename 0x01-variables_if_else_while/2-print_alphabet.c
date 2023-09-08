6c72245f921d6177e614807e177e2581b5a5433d
hima890 <hfibrahim90@gmail.com>
2023-09-08 15:17:20 +0200

    Add file 2 6

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,7 +12,7 @@ int main(void)
 {
 char first_letter;
 /* your code goes there */
-for (first_letter = 'a'; first_letter < 'z'; first_letter++)
+for (first_letter = 'a'; first_letter <= 'z'; first_letter++)
 {
 putchar(first_letter);
 }
