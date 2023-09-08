452f786fb7590b670230c6d17330aa9e9dd6656e
hima890 <hfibrahim90@gmail.com>
2023-09-08 16:47:20 +0200

    Add file 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int first_digit;
+int secund_digit;
+/* your code goes there */
+for (first_digit = 0; first_digit <= 9; first_digit++)
+{
+for (secund_digit = first_digit + 1; secund_digit <= 9; secund_digit++)
+{
+putchar('0' + first_digit);
+putchar('0' + secund_digit);
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
