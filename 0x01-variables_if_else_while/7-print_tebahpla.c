1b8abfcfa9fcdb7d8924fdce2e25177e229d454d
hima890 <hfibrahim90@gmail.com>
2023-09-08 16:02:20 +0200

    Add file 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include<stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char first_letter;
+/* your code goes there */
+for (first_letter = 'z'; first_letter >= 'a'; first_letter--)
+{
+putchar(first_letter);
+}
+putchar('\n');
+return (0);
+}
