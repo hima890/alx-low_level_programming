659818a2b19fbb4351e206c1ae4dd183cfd1646e
hima890 <hfibrahim90@gmail.com>
2023-09-12 12:56:39 +0200

    Update file 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,14 +7,17 @@
  * Return: Always 0 (Success)
  * printing "_putchar" using for loop
  */
+
 int main(void)
 {
-    char strings[9] = "_putchar";
-    int i;
-    for (i = 0; i <= 9; i++)
-    {
-        _putchar(strings[i]);
-    }
-    return (0);
 
+char strings[9] = "_putchar";
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+_putchar(strings[i]);
+}
+
+return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
- * Declare the _putchar function
- * 
-*/
+ * _putchar - Writes a character to the standard output.
+ *
+ * @c: The character to be written.
+ *
+ * Return: On success, 1. On error, -1 is returned,
+ *  and errno is set appropriately.
+ */
 
 int _putchar(char c);
-
 #endif /* MYHEADER_H */
