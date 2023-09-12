696d591ed65cac71412ff4f9c7f26c1da7495efb
hima890 <hfibrahim90@gmail.com>
2023-09-12 18:31:26 +0200

    add file 12

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,10 +14,21 @@ for (tabel_namber = 0; tabel_namber <= 10; tabel_namber++)
 {
 for (number = 0; number < 10; number++)
 {
+if ((tabel_namber * number) > 9)
+{
+_putchar('0' + ((tabel_namber * number) / 10));
+_putchar('0' + ((tabel_namber * number) % 10));
+_putchar(',');
+_putchar(' ');
+}
+else
+{
+_putchar('0' + (tabel_namber * number));
 _putchar('0' + (tabel_namber * number));
 _putchar(',');
 _putchar(' ');
 }
 _putchar('\n');
+}
 }
 }
