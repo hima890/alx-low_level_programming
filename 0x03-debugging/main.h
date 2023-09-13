eecc0a4739f2e552978cb856e32e916aeb43028b
hima890 <hfibrahim90@gmail.com>
2023-09-13 13:05:26 +0200

    add file 1 6

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,10 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-/**
- * positive_or_negative - Writes a character to the standard output.
- * @i: Integer
- */
+
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
