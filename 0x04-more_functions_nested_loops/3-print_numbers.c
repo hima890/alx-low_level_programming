570336b0c6038989dd3622fd9b62ec5be75f1e07
hima890 <hfibrahim90@gmail.com>
2023-09-14 12:02:24 +0200

    add file 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/* more headers goes there */
+
+/**
+ * print_numbers - function that multiplies two integers.
+ * Return: number 0 -9 and new line
+*/
+
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+_putchar(i - '0');
+}
+_putchar('/n');
+}
