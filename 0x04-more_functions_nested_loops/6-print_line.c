91209f18007f64d92f64a815c4a2ad5ac5cb9933
hima890 <hfibrahim90@gmail.com>
2023-09-14 15:35:49 +0200

    add file 6 1

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,20 +3,21 @@
 
 /**
  * print_line- function that draws a straight line in the terminal.
- */
+*/
+
 void print_line(int n)
 {
 int i;
 if (n <= 0)
 {
-putchar('\n');
+_putchar('\n');
 }
 else
 {
 for (i = 0; i < n; i++)
 {
-putchar('_');
+_putchar('_');
 }
-putchar('\n');
+_putchar('\n');
 }
 }
