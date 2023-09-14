c6b7222ec9e190f5f54af4ea89fef8b5ddfa1fe4
hima890 <hfibrahim90@gmail.com>
2023-09-14 15:07:47 +0200

    add file 5 2

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,18 +1,23 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
-  * print_most_numbers - Print the numbers since 0 up to 14
-*/
-void print_most_numbers(void)
+ * more_numbers - Print the numbers from 0 to 14, repeated 10 times
+ */
+void more_numbers(void)
 {
 int x, i;
+
 for (i = 0; i < 10; i++)
 {
-for (x = 0; x < 15; x++)
+for (x = 0; x <= 14; x++)
 {
-_putchar(x + '0');
-
+if (x >= 10)
+{
+putchar(x / 10 + '0');
 }
-_putchar('\n');
+putchar(x % 10 + '0');
+}
+putchar('\n');
 }
 }
