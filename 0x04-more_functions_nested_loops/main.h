840674371b6d4abfc9ca5acb73bfde4e315723fa
hima890 <hfibrahim90@gmail.com>
2023-09-14 14:48:57 +0200

    add file 4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
- * _isupper - checks for uppercase character.
-*/
 
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 #endif /* MYHEADER_H */
