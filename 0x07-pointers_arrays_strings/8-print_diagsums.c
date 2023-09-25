1719b951c1c6eef5a97c0b644c9f815e26643bcb
hima890 <hfibrahim90@gmail.com>
2023-09-25 18:03:50 +0200

    file 8 3

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,13 +9,12 @@ void print_diagsums(int *a, int size)
 {
 int diag1_sum = 0;
 int diag2_sum = 0;
-
-for (int i = 0; i < size; i++)
+int i;
+for (i = 0; i < size; i++)
 {
 diag1_sum += a[i * size + i];
 diag2_sum += a[i * size + (size - 1 - i)];
 }
 
-printf("Sum of main diagonal: %d\n", diag1_sum);
-printf("Sum of secondary diagonal: %d\n", diag2_sum);
+printf("%d, %d\n", diag1_sum, diag2_sum);
 }
