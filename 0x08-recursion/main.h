f50c7565356679a03a46c32bfb84b9b1d1633a1c
hima890 <hfibrahim90@gmail.com>
2023-09-28 20:12:55 +0200

    file 6

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,4 +9,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt_helper(int n, int guess);
+int is_prime_number(int n);
+int is_prime_helper(int n, int divisor);
 #endif
