551a71a03de3f424dfe3c8af1881ce26f6ced853
hima890 <hfibrahim90@gmail.com>
2023-10-02 19:38:52 +0200

    add file 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h> 
+
+/**
+ * main - Entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line argument strings
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+int multiplication;
+int num1;
+int num2;
+num1= atoi(argv[2]);
+num2= atoi(argv[1]);
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+
+multiplication = num1 * num2;
+
+printf("%d\n", multiplication);
+
+return (0);
+}
