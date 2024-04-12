8d7a5d7c4f11709b1f87c06719106ff8f74d1885
hima890 <hfibrahim90@gmail.com>
2024-04-12 18:23:25 +0200

    Add al the task 0-6 and the header file

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,53 @@
+#include "hash_tables.h"
+#include <stdio.h>
+/**
+ * hash_table_set - Adds an element to the hash table
+ * @ht: The hash table
+ * @key: The key
+ * @value: The value associated with the key
+ *
+ * Return: 1 if successful, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node, *temp_node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	temp_node = ht->array[index];
+	while (temp_node != NULL)
+	{
+		if (strcmp(temp_node->key, key) == 0)
+		{
+			/* Update value if key already exists */
+			free(temp_node->value);
+			temp_node->value = strdup(value);
+			if (temp_node->value == NULL)
+				return (0);
+			return (1);
+		}
+		temp_node = temp_node->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
